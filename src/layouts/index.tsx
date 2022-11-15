bed58bced0cee3c980b9ba7eaaf99758c261fb28
yangli <172209668@qq.com>
2022-11-15 23:02:19 +0800

    feat: add features into liquidity component

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -9,7 +9,7 @@ import './index.less';
 import Icon from '@/components/Icon';
 import IconWithoutBack from '@/components/IconWithoutBack';
 
-import logo from '../assets/logo/logo-with-text.svg';
+import  Header  from './Header';
 
 const items2: MenuProps['items'] = [
   { label: 'Trading', svg: 'trade' },
@@ -49,46 +49,7 @@ const items3: MenuProps['items'] = [
 
 const App: React.FC = () => (
   <Layout className="outside-layout">
-    <header className="header">
-      <div>
-        <Row className="header-row" justify="space-between" align="middle">
-          <Col className="header-row-col" span={4}>
-            <img
-              src={logo}
-              style={{ verticalAlign: 'middle', color: 'rgba(59,130,246,.5)' }}
-            />
-          </Col>
-          <Col className="header-row-col">
-            <Row justify="space-between" align="middle" style={{ gap: '2rem' }}>
-              <Col style={{ width: 24, height: 24 }}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                  ></path>
-                </svg>
-              </Col>
-              <Col>
-                <Button
-                  className="header-row-col-button frosted-glass frosted-glass-teal rounded-xl "
-                  style={{ width: 160, height: 44 }}
-                >
-                  Connect Wallet
-                </Button>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </div>
-    </header>
+   <Header/>
     <Layout className="main-layout">
       <Row style={{ backgroundColor: '#131a35' }}>
         <Col>
