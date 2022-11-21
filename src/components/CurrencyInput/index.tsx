2bcfa1d85dd2993c827bd5b4a3628329363c8bc0
tangliang1 <tangliang@hinterstellar.com>
2022-11-21 12:46:49 +0800

    fix(liquidity): fix debounce of currency input

--- src/components/CurrencyInput/index.tsx
+++ src/components/CurrencyInput/index.tsx
@@ -1,45 +1,68 @@
-import React, { useCallback, useState } from "react";
-import Styles from './index.less'
-import { inputDebounce } from "@/utils/inputDebounce";
-import classNames from 'classnames/bind'
-const cx = classNames.bind(Styles)
+import classNames from 'classnames/bind';
+import React, { ChangeEvent, useCallback, useEffect, useState } from 'react';
+import Styles from './index.less';
+const cx = classNames.bind(Styles);
 
-interface IInputProps{
-  onChange: (v: string| number)=>void;
-  symbol?: string
+interface IInputProps {
+  onChange: (v: string | number) => void;
+  symbol?: string;
   icon?: string;
-  value?: number;
-  balance?: number // undefined indicates no wallet connected. 0 means balance is 0 
+  value?: number | string;
+  balance?: number; // undefined indicates no wallet connected. 0 means balance is 0
 }
 
-export default function CurrencyInput(props: IInputProps ) {
-  const debounce = inputDebounce((cur)=>{
-    props.onChange(cur);
-  },200);
-  const handleInput = useCallback((e)=>{
-    // 非受控组件
-    let nowCur = e.target.value;
-    if(/^\d+\.?\d*$/.test(nowCur)) {
-      debounce(nowCur);
+const useDebounce = (delay: number) => {
+  let timmer;
+  // prevent memory leak: there is only one debounce instance
+  const [state] = useState({
+    debounce: (callback: () => void) => {
+      if (timmer) clearTimeout(timmer);
+      timmer = setTimeout(callback, delay);
+    },
+  });
+  useCallback(() => {
+    // clear timmer before component destroy
+    return () => {
+      if (timmer) clearTimeout(timmer);
+    };
+  }, []);
+  return state.debounce;
+};
+
+export default function CurrencyInput(props: IInputProps) {
+  // const debounce = inputDebounce((cur) => {
+  //   props.onChange(cur);
+  // }, 200);
+  const debounce = useDebounce(200);
+  const [value, setValue] = useState(props.value || '');
+  useEffect(() => {
+    // if inpunt is foucs, omit the value from parent
+    // if (inputRef.current?.focus) return;
+    setValue(props.value || '');
+  }, [props.value]);
+
+  const handleInput = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const currentValue = e.target.value;
+    if (currentValue === '' || /^\d+\.?\d*$/.test(currentValue)) {
+      setValue(currentValue);
+      debounce(() => props.onChange(currentValue));
+    }
+  }, []);
+
+  const handlePreset = useCallback((type: 'max' | 'half') => {
+    if (!props.balance) return;
+    if (type === 'max') {
+      props.onChange(props.balance);
     } else {
-      nowCur = nowCur.replace(/[^.\d]/,'').replace(/(\d+)(\.)(\d*)(\.)/,'$1$2$3')
-      e.target.value = nowCur;
+      props.onChange(props.balance / 2);
     }
+  }, []);
 
-  }, [])
-
-  const handlePreset = useCallback((type: 'max' | 'half')=>{
-    if(!props.balance)return;
-    if(type === 'max'){
-      props.onChange(props.balance)
-    }else{
-      props.onChange(props.balance/2)
-    }
-  }, [])
-  
   return (
     <div className={cx('bp-inner-panel')}>
-      <div className={cx('bp-inner-panel-row')}>{`Balance: (Wallet not connected)`}</div>
+      <div
+        className={cx('bp-inner-panel-row')}
+      >{`Balance: (Wallet not connected)`}</div>
       {/* 第二层 */}
       <div className={cx('bp-inner-panel-row')}>
         {/* 图标 */}
@@ -49,26 +72,46 @@ export default function CurrencyInput(props: IInputProps ) {
               <img className={cx('bpinner-img')} src={props.icon} alt="" />
             </div>
             <div className={cx('icon-label')}>{props.symbol}</div>
-            <div style={{width: 12}}>
-              <img src={require('@/assets/icons/msic-exchange.svg').default} alt="" />
+            <div style={{ width: 12 }}>
+              <img
+                src={require('@/assets/icons/msic-exchange.svg').default}
+                alt=""
+              />
             </div>
           </div>
         </div>
         {/* 分割线 */}
-        <div className={cx("bpinner-panel-divider")}></div>
+        <div className={cx('bpinner-panel-divider')}></div>
         {/* 两个小按钮 */}
-        <div className={cx("bpinner-button-input")}>
-          <div className={cx("bpinner-button-wrap")}>
-            <button type="button" className={cx("bpinner-button")} onClick={()=>handlePreset('max')}>Max</button>
-            <button type="button" className={cx("bpinner-button")} onClick={()=>handlePreset('half')}>Half</button>
-          </div> 
-          <div >
-            <input className={cx("bpinner-panel-input")} placeholder="" defaultValue={props.value?.toString()} onChange={handleInput}></input>
+        <div className={cx('bpinner-button-input')}>
+          <div className={cx('bpinner-button-wrap')}>
+            <button
+              type="button"
+              className={cx('bpinner-button')}
+              onClick={() => handlePreset('max')}
+            >
+              Max
+            </button>
+            <button
+              type="button"
+              className={cx('bpinner-button')}
+              onClick={() => handlePreset('half')}
+            >
+              Half
+            </button>
+          </div>
+          <div>
+            <input
+              className={cx('bpinner-panel-input')}
+              placeholder=""
+              value={value}
+              onChange={handleInput}
+            ></input>
           </div>
         </div>
       </div>
       {/* 第三层 */}
-      <div className={cx("bp-inner-panel-row")}>
+      <div className={cx('bp-inner-panel-row')}>
         <span>--</span>
       </div>
     </div>
--- src/pages/liquidity/index.tsx
+++ src/pages/liquidity/index.tsx
@@ -1,85 +1,92 @@
 import { Progress } from 'antd';
+import { shallowEqual, useDispatch, useSelector } from 'dva';
 import { useCallback, useState } from 'react';
-import { useSelector, useDispatch, shallowEqual } from 'dva'
 
 import BluePurplePanel from '@/components/BluePurplePanel';
-import CurrencyInput from '@/components/CurrencyInput';
 import ButtonWithBorder from '@/components/ButtonWithBorder';
-import SwitchTabs from '@/components/SwitchTabs';
 import CurrencyDataTable from '@/components/CurrencyDataTable';
+import CurrencyInput from '@/components/CurrencyInput';
+import SwitchTabs from '@/components/SwitchTabs';
 
-import {ILiquidityState} from './model'
+import { ILiquidityState } from './model';
 
-import Styles from './index.less'
-import classNames from 'classnames/bind'
-const cx = classNames.bind(Styles)
+import classNames from 'classnames/bind';
+import Styles from './index.less';
+const cx = classNames.bind(Styles);
 
 interface ICurrency {
   symbol?: string;
   icon?: string;
-  amount?: number;
+  amount?: number | string;
 }
 
-
-const useCurrency = (base: ICurrency, quote: ICurrency)=>{
-  const [baseState, setBaseState] =  useState<ICurrency>({
+const useCurrency = (base: ICurrency, quote: ICurrency) => {
+  const [baseState, setBaseState] = useState<ICurrency>({
     icon: base.icon,
     amount: base.amount,
-    symbol: base.symbol
+    symbol: base.symbol,
   });
-  const [quoteState, setQuoteState] =  useState<ICurrency>({
+  const [quoteState, setQuoteState] = useState<ICurrency>({
     icon: quote.icon,
     amount: quote.amount,
-    symbol: quote.symbol
+    symbol: quote.symbol,
   });
 
   return {
     baseCurrencyState: baseState,
     quoteCurrencyState: quoteState,
-    changeAmount: (baseAmount: number, quoteAmount: number)=>{
+    changeAmount: (
+      baseAmount: number | string,
+      quoteAmount: number | string,
+    ) => {
       setBaseState({
         amount: baseAmount,
         symbol: baseState.symbol,
         icon: baseState.icon,
-      })
+      });
       setQuoteState({
         amount: quoteAmount,
         symbol: quoteState.symbol,
         icon: quoteState.icon,
-      })
-    }
-  }
-}
-
+      });
+    },
+  };
+};
 
 export default function Liquidity() {
-
   const dispatch = useDispatch();
 
-  const {poolInfo} = useSelector((state:{liquidity: ILiquidityState})=>{
-    return state.liquidity
+  const { poolInfo } = useSelector((state: { liquidity: ILiquidityState }) => {
+    return state.liquidity;
   }, shallowEqual);
 
-  const {baseCurrencyState, quoteCurrencyState, changeAmount} = useCurrency(
-    {symbol: poolInfo?.baseToken.symbol, icon: poolInfo?.baseIcon}, 
-    {symbol: poolInfo?.quoteToken.symbol, icon: poolInfo?.quoteIcon}, 
-    );
+  const { baseCurrencyState, quoteCurrencyState, changeAmount } = useCurrency(
+    { symbol: poolInfo?.baseToken.symbol, icon: poolInfo?.baseIcon },
+    { symbol: poolInfo?.quoteToken.symbol, icon: poolInfo?.quoteIcon },
+  );
 
-  const onCurrencyInputChange = (direction: 'base' | 'quote', value: number)=>{
-    if(!poolInfo){
-      return
+  const onCurrencyInputChange = (
+    direction: 'base' | 'quote',
+    value: number | string,
+  ) => {
+    if (!poolInfo) {
+      return;
     }
-     // normal number 
-     let rate = poolInfo?.rate;
-     if(direction === 'base'){
-       changeAmount(value,value*rate)
-     }else{
-       changeAmount(value/rate, value)
-     }
-  }
+    // normal number
+    const rate = poolInfo?.rate;
+    if (value === '') {
+      // clear input
+      changeAmount('', '');
+    } else {
+      value = Number(value);
+      if (direction === 'base') {
+        changeAmount(value, value * rate);
+      } else {
+        changeAmount(value / rate, value);
+      }
+    }
+  };
 
-
-  
   return (
     <div className={cx('liquidity-box')}>
       <div className={cx('switch-tabs-wrapper')}>
@@ -88,21 +95,25 @@ export default function Liquidity() {
       <div>
         <BluePurplePanel>
           {/* 第一个input */}
-          <div className={cx('bpinnerpanel-wrap','wrap1')}>
-            <CurrencyInput 
-            value={baseCurrencyState.amount} 
-            symbol={baseCurrencyState.symbol}
-            icon={baseCurrencyState.icon} 
-            onChange={(value)=>onCurrencyInputChange('base', value)}/>
+          <div className={cx('bpinnerpanel-wrap', 'wrap1')}>
+            <CurrencyInput
+              value={baseCurrencyState.amount}
+              symbol={baseCurrencyState.symbol}
+              icon={baseCurrencyState.icon}
+              onChange={(value) => onCurrencyInputChange('base', value)}
+            />
           </div>
           {/* 中间间隔区域 */}
           <div className={cx('partition-box')}>
             <div className={cx('partition-box-left')}>
               <div className={cx('parition-box-img')}>
-                <img src={require('@/assets/icons/msic-plus.svg').default} alt="--" />
+                <img
+                  src={require('@/assets/icons/msic-plus.svg').default}
+                  alt="--"
+                />
               </div>
               <div>
-                <div className={cx('flex','text-ABC4FF',' font-size-14px')}>
+                <div className={cx('flex', 'text-ABC4FF', ' font-size-14px')}>
                   {`1 ${baseCurrencyState.symbol} ≈ ${poolInfo?.rate} ${quoteCurrencyState.symbol}`}
                   <div> &nbsp;⇋</div>
                 </div>
@@ -110,11 +121,19 @@ export default function Liquidity() {
             </div>
             {/* 放大镜 进度条 */}
             <div className={cx('partition-box-right')}>
-              <div className={cx('parition-box-img')} >
-                <img src={require('@/assets/icons/msic-search.svg').default} alt="--" />
+              <div className={cx('parition-box-img')}>
+                <img
+                  src={require('@/assets/icons/msic-search.svg').default}
+                  alt="--"
+                />
               </div>
               <div className={cx('text-ABC4FF')}>
-                <Progress type="circle" percent={30} format={percent => ''} width={24}  />
+                <Progress
+                  type="circle"
+                  percent={30}
+                  format={(percent) => ''}
+                  width={24}
+                />
               </div>
             </div>
           </div>
@@ -124,7 +143,8 @@ export default function Liquidity() {
               value={quoteCurrencyState.amount}
               symbol={quoteCurrencyState.symbol}
               icon={quoteCurrencyState.icon}
-              onChange={(value)=>onCurrencyInputChange('quote', value)}/>
+              onChange={(value) => onCurrencyInputChange('quote', value)}
+            />
           </div>
           {/* data table */}
           <CurrencyDataTable></CurrencyDataTable>
